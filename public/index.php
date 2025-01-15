74a091bfc977532502ced4b97aa3b8f99bb824f0
Charles Marks <charles_marks04@tamu.edu>
2025-01-14 22:28:04 -0600

    First Day Progress

--- public/index.php
+++ public/index.php
@@ -12,8 +12,7 @@
 
 	echo "<h1> Charles' CSCE 331 Docker Web Site </h1>\n";
 	
-	include 'proc_csv.php'; //making index require proc csv so it has access
-   	
+	require_once("proc_csv.php");   	
 	echo "<h3> Single Quote, Tab CSV, Displaying Columns 2 and 4 </h3>\n";
 	proc_csv("dat2-singlequote-tab.csv", "\t" , "\'" , "2:4"); //calling proc_csv
 	echo "<h3> Double Quote, Tab CSV, Displaying Columns 1 and 3 </h3>\n";
