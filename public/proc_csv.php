d842743b0eca421ac0f9386175e7809cb8b59b9a
Charles Marks <charles_marks04@tamu.edu>
2025-01-14 22:09:21 -0600

    First Day Progress - Submission 1 Complete

--- public/proc_csv.php
+++ public/proc_csv.php
@@ -1,7 +1,7 @@
 <?php
    function proc_csv($filename, $delimiter, $quote, $columns_to_show) {
                $handle = fopen($filename, "r") or die("Cannot Open CSV file");
-               $column_numbers = explode(":", $columns_to_show);
+               $column_numbers = preg_split("/:/", $columns_to_show); //creates an array of column values
                echo "<table  border=\"1\">\n";
                if($delimiter === "\t")
                     $delimiter = '\t(?=(?:[^"]*"[^"]*")*[^"]*$)'; //delimiter to select all tabs that arent included in quotes
@@ -10,18 +10,18 @@
                $firstPass = true;
                while($data = fgets($handle)) {
                        echo "<tr>\n";
-                       $data_cols = preg_split("/" . $delimiter . "/", $data);
+                       $data_cols = preg_split("/" . $delimiter . "/", $data); //splits $data (horizontal lines of csv file) into individual array elements split by delimiter
                        for($k=0; $k<count($data_cols); ++$k) {
                                 if($firstPass === true) {
-					if(in_array((string)($k+1), $column_numbers) || $columns_to_show === "ALL")
-                                        echo " <td> <b> ".preg_replace("/".$quote."/", "", $data_cols[$k] )." </b> </td>\n"; //prints the first item of each column in bold
+					if(in_array((string)($k+1), $column_numbers) || $columns_to_show === "ALL") //checks if $k+1 (column number) is equal to some value in $column_numbers (array of columns to show values) or if columns to show is ALL
+						echo " <td> <b> ".preg_replace("/".$quote."/", "", $data_cols[$k] )." </b> </td>\n"; //prints the first item of each column in bold
                                 }
                                 else {
 					if(in_array((string)($k+1), $column_numbers) || $columns_to_show === "ALL")
-                                        echo "  <td> ".preg_replace("/".$quote."/", "", $data_cols[$k] )." </td>\n";
+                                        	echo "  <td> ".preg_replace("/".$quote."/", "", $data_cols[$k] )." </td>\n";
                        		}
 		       }
-                       $firstPass = false; //boolean to keep track of if it is the first item in each column
+                       $firstPass = false; //boolean to keep track of column headers for them to be bold
                        echo "</tr>\n";
                }
                fclose($handle);
