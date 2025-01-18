42473e80bad83b7f42a60518f7d2cd08550ed2be
Charles Marks <charles_marks04@tamu.edu>
2025-01-17 18:40:27 -0600

    Added Comments to Markdown Code

--- public/proc_Markdown.php
+++ public/proc_Markdown.php
@@ -10,17 +10,20 @@
 			if($data == "\n")
 				$data = "<p>";			
 
-			$data = preg_replace("/^# (.*)/", "<h1> $1 </h1>" , $data);
-			$data = preg_replace("/^## (.*)/", "<h2> $1 </h2>" , $data);
-			$data = preg_replace("/^### (.*)/", "<h3> $1 </h3>" , $data);
+			$data = preg_replace("/^# (.*)/", "<h1> $1 </h1>" , $data); //First Level Heading
+			$data = preg_replace("/^## (.*)/", "<h2> $1 </h2>" , $data); //Second Level Heading
+			$data = preg_replace("/^### (.*)/", "<h3> $1 </h3>" , $data); //Third Level Heading
 
-			$data = preg_replace("/!\[(.+?)\]\((.+?)\)/", "<img src=$2 alt= $1 ></img>", $data);
-			$data = preg_replace("/\[(.+?)\]\((.+?)\)/", "<a href=$2> $1 </a>", $data);
+			$data = preg_replace("/!\[(.+?)\]\((.+?)\)/", "<img src=$2 alt= $1 ></img>", $data); //Image
+			$data = preg_replace("/\[(.+?)\]\((.+?)\)/", "<a href=$2> $1 </a>", $data); //URL
 
 
-			$data = preg_replace("/\*\*(.+?)\*\*/", "<b>$1</b>", $data);
-			$data = preg_replace("/_(.+?)_/", "<i>$1</i>", $data);
+			$data = preg_replace("/\*\*(.+?)\*\*/", "<b>$1</b>", $data); //Bold Text
+			$data = preg_replace("/_(.+?)_/", "<i>$1</i>", $data); //Italics Text
 
+
+
+			//WORK IN PROGRESS LISTS
 			$data = preg_replace("/^\* (.*)/",  "$1" , $data);
 
 
