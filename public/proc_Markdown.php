f1da6bca8bc024905658016bad5db84ae80f84fc
Charles Marks <charles_marks04@tamu.edu>
2025-01-17 20:11:19 -0600

    Finished Unordered List

--- public/proc_Markdown.php
+++ public/proc_Markdown.php
@@ -3,11 +3,22 @@
 	function proc_markdown($filename){
 	
 		$handle = fopen($filename, "r") or die("Cannot Open MD file");
-		$in_list = false;
-		$list_data = null;
-		while($data = fgets($handle)) {
+		$in_ulist = false;
+		$in_usublist = false;
+		while($data = fgets($handle))
+		{
 			if($data == "\n")
-				$data = "<p>";			
+			{
+
+				if ($in_ulist) 
+				{
+                			echo "</ul>";
+                			$in_ulist = false;
+            			}
+				
+				echo "</p> <p>";
+				continue;			
+			}
 
 			$data = preg_replace("/^\s*# (.*)/", "<h1> $1 </h1>" , $data); //First Level Heading
 			$data = preg_replace("/^\s*## (.*)/", "<h2> $1 </h2>" , $data); //Second Level Heading
@@ -21,16 +32,38 @@
 			$data = preg_replace("/_(.+?)_/", "<i>$1</i>", $data); //Italics Text
 
 
+			if(preg_match("/^\s*\* (.*)/" , $data)) //Unordered List
+			{
+				if(!$in_ulist)
+				{
+					echo "<ul>";
+					$in_ulist = true;
+				}
+				if(preg_match("/^ {4}\* (.*)/", $data))
+				{
+					if(!$in_usublist)
+                                	{
+                                        	echo "<ul>";
+                                        	$in_usublist = true;
+                                	}
 
-			//WORK IN PROGRESS LISTS
-			$data = preg_replace("/^\* (.*)/",  "$1" , $data);
+				}
+				else if($in_usublist)
+				{
+					echo "</ul>";
+					$in_usublist = false;
+				}
+				$data = preg_replace("/^\s*\* (.*)/",  "<li>$1</li>" , $data);
+			}
 
-
-			echo $data;
+			echo $data;			
+	
 
 
 		}	
 		fclose($handle);
+		if($in_ulist)
+                	echo "</ul>";
 	}
 
 ?>
