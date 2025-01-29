b198973fcc90e23abdc1f216c1fe4cd00c49beab
Charles Marks <charles_marks04@tamu.edu>
2025-01-29 14:10:10 -0600

    Changed some Layout of Code for Personal Taste

--- public/index.php
+++ public/index.php
@@ -13,9 +13,8 @@
 	echo "<h1> Charles' CSCE 331 Docker Web Site </h1>\n";
 
 ?>
-
-	<a href=http://localhost:5555/proc_markdown.php> Git Markdown </a><br>
 	<a href=http://localhost:5555/proc_csv.php> Display CSV Tables </a><br>
+	<a href=http://localhost:5555/proc_markdown.php> Git Markdown </a><br>
 	<a href=http://localhost:5555/search.php> Search Page </a>
 
 </body>
