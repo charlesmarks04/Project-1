4fa14b4e726c7f122cc94170bc9c1043d70c0a5d
Charles Marks <charles_marks04@tamu.edu>
2025-01-29 12:23:34 -0600

    I Think I finished Search

--- public/search.php
+++ public/search.php
@@ -8,7 +8,6 @@
 
 
 <body>
-	<a href=http://localhost:5555> Back </a><br>	
 	<h1>Website Search</h1>
     		<form action="search.php" method="get">
         		<input type="text" name="keyword" placeholder="Enter search term..." required>
@@ -17,16 +16,17 @@
 <?php
 $somethingFound = false;
 $searchingFor = $_GET["keyword"];
-if (filter_var($searchingFor,FILTER_DEFAULT)) {
-	echo "<h2> Showing Results for '" . $searchingFor . "'... <h2>";
-	$pages = ["/index.php", "/proc_csv.php", "/proc_markdown.php"];
+if (filter_var($searchingFor,FILTER_SANITIZE_STRING)) {
+	echo "<h2> Showing Results for '" . $searchingFor . "'... </h2>";
+	$pages = ["index.php", "proc_csv.php", "proc_Markdown.php"];
 	foreach($pages as $page){
 		
-		//$url = "http://localhost:5555/" . $page;
-		$html = file_get_contents("http://host.docker.internal:5555/index.php");
-		print_r($html);
-		if(str_contains($html,$searchingFor)){
-			echo "<h3> Found ". $searchingFor . " at <a " .$searchingFor. ">".$url." </a><h3>";
+		$internal_url = "http://host.docker.internal:5555/" . $page;
+		$external_url = "http://localhost:5555/" . $page;
+		$html = file_get_contents($internal_url);
+		//print_r($html);
+		if(strpos($html,$searchingFor)){
+			echo "<h3>Found '". $searchingFor . "' at <a href= " .$external_url. ">".$page." </a></h3>";
 			$somethingFound = true;
 		}
 
@@ -35,11 +35,16 @@ if (filter_var($searchingFor,FILTER_DEFAULT)) {
 
 	}
 	if($somethingFound == false){
-		echo "<h3> No Results for '".$searchingFor."'... <h3";
+		echo "<h3> No Results for '".$searchingFor."'... </h3>";
 	}
+}
+else{
+	$display = str_replace("<", "&lt", $searchingFor);
+	echo "<h3> Please Enter a Valid Search. ".$display." is not a Valid Search </h3>";
 }	
 ?>
 
+	<a href=http://localhost:5555> Back </a><br>
 </body>
 
 
