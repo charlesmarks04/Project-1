f4078d6519db25407bfde9c61a9c05290594bdbd
Charles Marks <charles_marks04@tamu.edu>
2025-02-04 01:25:58 -0600

    Made the Website look more like a website

--- public/index.php
+++ public/index.php
@@ -11,32 +11,14 @@
 <?php
 
 	echo "<h1> Charles' CSCE 331 Docker Web Site </h1>\n";
-	
-
-	require_once("proc_csv.php");
-        echo "<h1><ins> CSV Tables </ins></h1>";
-	echo "<h3> Single Quote, Tab CSV, Displaying Columns 2 and 4 </h3>\n";
-        	proc_csv("dat2-singlequote-tab.csv", "\t" , "\'" , "2:4"); //calling proc_csv
-        echo "<h3> Double Quote, Tab CSV, Displaying Columns 1 and 3 </h3>\n";
-        	proc_csv("dat2-doublequote-tab.csv", "\t" , "\"" , "1:3"); //calling proc_csv
-        echo "<h3> Double Quote, Comma CSV, Displaying ALL Columns </h3>\n";
-        	proc_csv("dat-doublequote-comma.csv", "," , "\"" , "ALL"); //calling proc_csv
-
-	require_once("proc_Markdown.php");
-	echo "<h1><ins> Git Markdown </ins></h1>";
-                proc_markdown("markdown.md");
-
-	require_once("proc_gallery.php");
-        echo "<h1><ins> Gallery </ins></h1>";
-                proc_gallery("Image_CSV.csv", "list", "orig");
-		proc_gallery("Image_CSV.csv", "matrix", "orig");
-		proc_gallery("Image_CSV.csv", "list", "size_largest");
-		proc_gallery("Image_CSV.csv", "list", "size_smallest");
-		proc_gallery("Image_CSV.csv", "details", "size_smallest");
-
 
 ?>
+	<a href=http://localhost:5555/csv.php> CSV Table Page </a><br>
+	<a href=http://localhost:5555/markdown.php> Markdown Page </a><br>
+	<a href=http://localhost:5555/gallery.php> Gallery Page </a><br>
 	<a href=http://localhost:5555/search.php> Search Page </a><br>
+	
+
 
 </body>
 </html>
